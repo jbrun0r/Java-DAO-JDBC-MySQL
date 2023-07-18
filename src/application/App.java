c8539e59ccdf1279ff2c25fbb3dc4d008114c54d
João Rodrigues <joaobruno.rf@gmail.com>
2023-07-17 22:51:03 -0300

    feat: findById and findAll implementation

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,18 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class App {
 
 	public static void main(String[] args) {
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+//		Seller seller = sellerDao.findAll();
+		
+		System.out.println(sellerDao.findAll());
 
 	}
 
