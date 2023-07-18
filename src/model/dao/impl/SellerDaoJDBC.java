4c37387244a9ca5b3c8cca9600aa2bc49d457243
João Rodrigues <joaobruno.rf@gmail.com>
2023-07-17 21:57:29 -0300

    feat: SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,40 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(Seller seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller seller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
